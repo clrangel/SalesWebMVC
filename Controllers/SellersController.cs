a4273dbb8a5be9ad7e22d4cc8c48338b2a31c2f1
Camilo LR <autumndrums@hotmail.com>
2024-07-12 18:43:55 -0300

    Update seller and custom service exception

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -23,6 +24,7 @@ namespace SalesWebMvc.Controllers
 
             return View(list);
         }
+
         //CREATE
         // public IActionResult Create => View();
         public IActionResult Create()
@@ -80,6 +82,49 @@ namespace SalesWebMvc.Controllers
             }
 
             return View(obj);
+        }
+
+        //Edit
+        //Abre a tela para editar o vendedor
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
